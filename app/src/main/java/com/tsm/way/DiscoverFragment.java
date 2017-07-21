87240770b12c4c2497a8a11fb32faa3fdc75995e
Tonmoy <thTonmoy@users.noreply.github.com>
2017-07-22 00:14:44 +0600

    Map styled, fixed error

--- app/src/main/java/com/tsm/way/DiscoverFragment.java
+++ app/src/main/java/com/tsm/way/DiscoverFragment.java
@@ -1,18 +1,23 @@
 package com.tsm.way;
 
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.support.v4.app.FragmentActivity;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import static android.content.ContentValues.TAG;
 
 
 /**
@@ -21,10 +26,10 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class DiscoverFragment extends Fragment implements OnMapReadyCallback {
 
 
+    private GoogleMap mMap;
     public DiscoverFragment() {
         // Required empty public constructor
     }
-    private GoogleMap mMap;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -38,6 +43,19 @@ public class DiscoverFragment extends Fragment implements OnMapReadyCallback {
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        try {
+            // Customise the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            boolean success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            getContext(), R.raw.style_json));
+
+            if (!success) {
+                Log.e(TAG, "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e(TAG, "Can't find style. Error: ", e);
+        }
         mMap = googleMap;
 
         // Add a marker in Sydney, Australia, and move the camera.
