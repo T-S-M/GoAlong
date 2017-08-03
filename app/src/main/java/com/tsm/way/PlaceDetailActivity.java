824cc17bf386ff8ac1139a2692ea7a635f048fce
mahsin <mahsinbinakram@gmail.com>
2017-08-03 12:33:17 +0600

    Detail Activity added

---
+++ app/src/main/java/com/tsm/way/PlaceDetailActivity.java
@@ -0,0 +1,13 @@
+package com.tsm.way;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class PlaceDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place_detail);
+    }
+}
