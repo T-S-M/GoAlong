e0058d2ad344f9020b85ee8f62d94c7973b6f38f
mahsin <mahsinbinakram@gmail.com>
2017-08-27 23:39:38 +0600

    volley_request

--- app/src/main/java/com/tsm/way/ui/SavedPlacesFragment.java
+++ app/src/main/java/com/tsm/way/ui/SavedPlacesFragment.java
@@ -9,10 +9,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.facebook.AccessToken;
 import com.getbase.floatingactionbutton.FloatingActionButton;
 import com.tsm.way.R;
 import com.tsm.way.firebase.LinkFacebookActivity;
+import com.tsm.way.utils.PlaceListJSONParser;
 
 
 /**
@@ -39,6 +46,34 @@ public class SavedPlacesFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getActivity(), LinkFacebookActivity.class));
+            }
+        });
+
+        String YOUR_TOKEN = AccessToken.getCurrentAccessToken().toString();
+        String requestURL = "https://graph.facebook.com/search?&q=dhaka&type=event&fields=id,name,cover,start_time,end _time,description&access_token="+"YOUR_TOKEN";
+
+        final TextView textView = null;
+        textView.findViewById(R.id.text_json);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, requestURL,
+                new Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String response) {
+
+                        textView.setText("Response is: " + response.substring(0));
+                        //PlaceListJSONParser parser = new PlaceListJSONParser(response.substring(0), "restaurant");
+                        //try {
+                        //
+                        //
+                        //} catch (Exception e) {
+                        //e.printStackTrace();
+                        //}
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("That didn't work!");
             }
         });
         return rootView;
