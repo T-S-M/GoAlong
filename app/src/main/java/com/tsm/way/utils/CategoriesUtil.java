063edac9e11d5cab18864ee1729048ce25d69fe8
Sakib2263 <sakibhasan2263@gmail.com>
2017-08-31 00:12:43 +0600

    Vector assests and xml design only
    nothing much

--- app/src/main/java/com/tsm/way/utils/CategoriesUtil.java
+++ app/src/main/java/com/tsm/way/utils/CategoriesUtil.java
@@ -5,16 +5,17 @@ import com.tsm.way.R;
 public class CategoriesUtil {
     public static Category[] getCategories() {
         Category[] categories = {
-                new Category("Event", R.drawable.ic_restaurant_black_24dp),
+                new Category("Event", R.drawable.ic_local_activity_black_24dp),
                 new Category("Restaurant", R.drawable.ic_restaurant_black_24dp),
-                new Category("Cafe", R.drawable.ic_restaurant_black_24dp),
-                new Category("Park", R.drawable.ic_restaurant_black_24dp),
-                new Category("ATM", R.drawable.ic_restaurant_black_24dp),
-                new Category("Movie Theatre", R.drawable.ic_restaurant_black_24dp),
-                new Category("Museum", R.drawable.ic_restaurant_black_24dp),
-                new Category("Bar", R.drawable.ic_restaurant_black_24dp),
-                new Category("Spa", R.drawable.ic_restaurant_black_24dp),
-                new Category("Zoo", R.drawable.ic_restaurant_black_24dp)
+                new Category("Cafe", R.drawable.ic_local_cafe_black_24dp),
+                new Category("Park", R.drawable.ic_local_florist_black_24dp),
+                new Category("ATM", R.drawable.ic_local_atm_black_24dp),
+                new Category("Movie Theatre", R.drawable.ic_tv_black_24dp),
+                new Category("Museum", R.drawable.ic_account_balance_black_24dp),
+                new Category("Hotel", R.drawable.ic_hotel_black_24dp),
+                new Category("Bar", R.drawable.ic_local_bar_black_24dp),
+               new Category("Spa", R.drawable.ic_spa_black_24dp),
+               // new Category("Zoo", R.drawable.ic_restaurant_black_24dp)
         };
 
         return categories;
