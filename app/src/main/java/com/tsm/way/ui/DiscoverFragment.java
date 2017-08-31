6285cdcd05156ceec7e2801beec3d2fdc8f7e72a
Sakib2263 <sakibhasan2263@gmail.com>
2017-08-31 13:46:33 +0600

    Implemented 2 recyclerView(s)  and categories.
    Need to edit and work on this fragment ..maybe need another
    recyslerviewto hold whole fragment :/

--- app/src/main/java/com/tsm/way/ui/DiscoverFragment.java
+++ app/src/main/java/com/tsm/way/ui/DiscoverFragment.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,8 +14,21 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.GridView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.tsm.way.R;
+import com.tsm.way.model.PlaceBean;
 import com.tsm.way.utils.CategoriesUtil;
+import com.tsm.way.utils.PlaceListJSONParser;
+import com.tsm.way.utils.UrlsUtil;
+
+import java.util.List;
+
+import static com.tsm.way.ui.MainActivity.mLastKnownLocation;
 
 
 /**
@@ -21,7 +36,13 @@ import com.tsm.way.utils.CategoriesUtil;
  */
 public class DiscoverFragment extends Fragment {
 
+    public static final String TAG = "DiscoverFragment";
+
     private GridView categoriesGridView;
+    RecyclerView events_recyclerview,resturants_recyclerview;
+    RequestQueue mRequestQueue;
+    List<PlaceBean> placelist;
+    String type;
 
     public DiscoverFragment() {
         // Required empty public constructor
@@ -46,11 +67,65 @@ public class DiscoverFragment extends Fragment {
             }
         });
 
+        mRequestQueue = Volley.newRequestQueue(getContext());
+
+        events_recyclerview = (RecyclerView) rootView.findViewById(R.id.events_recyclerview);
+        events_recyclerview.setHasFixedSize(true);
+
+        resturants_recyclerview = (RecyclerView) rootView.findViewById(R.id.resturants_recyclerview);
+        resturants_recyclerview.setHasFixedSize(true);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        LinearLayoutManager layoutManager1 = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        events_recyclerview.setLayoutManager(layoutManager);
+        resturants_recyclerview.setLayoutManager(layoutManager1);
+
+        populateRecyclerview();
+
         return rootView;
     }
+    private void populateRecyclerview() {
 
+        String latitude = "23.8103";
+        String longitude = "90.4125";
+        if (mLastKnownLocation != null) {
+            latitude = String.valueOf(mLastKnownLocation.getLatitude());
+            longitude = String.valueOf(mLastKnownLocation.getLongitude());
+        }
+        if (type == null) {
+            type = "hospital";
+        }
 
+        String urlString1 = UrlsUtil.getCategoryPlaceUrlString(getContext(), latitude, longitude, type);
 
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, urlString1,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        //jsonTView.setText("Response is: " + response.substring(0));
+                        PlaceListJSONParser parser = new PlaceListJSONParser(response.substring(0), "restaurant");
+                        try {
+                            placelist = parser.getPlaceBeanList();
+                            events_recyclerview.setAdapter(new PlaceListAdapter(getContext(), placelist, null));
+                            events_recyclerview.setVisibility(View.VISIBLE);
+
+                            resturants_recyclerview.setAdapter(new PlaceListAdapter(getContext(), placelist, null));
+                            resturants_recyclerview.setVisibility(View.VISIBLE);
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //jsonTView.setText("That didn't work!");
+            }
+        });
+        // Add the request to the RequestQueue.
+        mRequestQueue.add(stringRequest);
+    }
 
 }
 
--- app/src/main/java/com/tsm/way/utils/CategoriesUtil.java
+++ app/src/main/java/com/tsm/way/utils/CategoriesUtil.java
@@ -6,7 +6,7 @@ public class CategoriesUtil {
     public static Category[] getCategories() {
         Category[] categories = {
                 new Category("Event", R.drawable.ic_local_activity_black_24dp),
-                new Category("Restaurant", R.drawable.ic_restaurant_black_24dp),
+                //new Category("Restaurant", R.drawable.ic_restaurant_black_24dp),
                 new Category("Cafe", R.drawable.ic_local_cafe_black_24dp),
                 new Category("Park", R.drawable.ic_local_florist_black_24dp),
                 new Category("ATM", R.drawable.ic_local_atm_black_24dp),
@@ -14,8 +14,8 @@ public class CategoriesUtil {
                 new Category("Museum", R.drawable.ic_account_balance_black_24dp),
                 new Category("Hotel", R.drawable.ic_hotel_black_24dp),
                 new Category("Bar", R.drawable.ic_local_bar_black_24dp),
-               new Category("Spa", R.drawable.ic_spa_black_24dp),
-               // new Category("Zoo", R.drawable.ic_restaurant_black_24dp)
+                new Category("Spa", R.drawable.ic_spa_black_24dp),
+                // new Category("Zoo", R.drawable.ic_restaurant_black_24dp)
         };
 
         return categories;
